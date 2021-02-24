9d4808a81decf0a2a731d2455971c8fb773ef14c
Shmn Riaz <71093488+Shmnriaz@users.noreply.github.com>
2021-02-25 02:48:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  // const [nayoks,setNayoks] = useState(0)
+ const [ persons, setPersons] = useState([])
+  useEffect(() => 
+  {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setPersons(data))
+  }, [])
+//https://randomuser.me/api/?results=5
+//https://jsonplaceholder.typicode.com/users
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        
+        {
+          persons.map(usr => <ProfileCard gender={usr.gender} postcode={usr.postcode}></ProfileCard>)
+          
+        }
+       
+        
       </header>
     </div>
   );
 }
 
+
+  function ProfileCard(props){
+    
+      const wordSpacing= {
+        wordSpacing:'80px'
+      }
+      const profCard = {
+        backgroundColor:'gray',
+        borderRadius:'20px'
+
+      }
+      const nameStyle={
+        padding:'3px',
+        color:'blue'
+      }
+    return(
+      <div style={profCard}>
+        <div ><img src="picture.thumbnail"></img></div>
+        <h6>  location.city location.state location.country</h6>
+        <div style={nameStyle}><h3></h3></div>
+        <div style={wordSpacing}><p>dob.email cell</p></div>
+        <p>Gender: {props.gender} </p>
+        <p> Age: registered.age {props.postcode}</p>
+
+
+      </div>
+    )
+  }
+
+// function MovieCounter() { 
+//   const [ count,setCount ] = useState(5);
+//   const handleClick = () => setCount(count+1);
+//   const handleDeClick = () => setCount(count-1);
+//   return(
+//     <div>
+//       <button  onClick={handleClick}>Add Movie</button>
+//       <button  onClick={handleDeClick}>Remove Movie</button>
+//       <h5>Number of Movies: {count}</h5>
+      
+//     </div>
+//   )
+//  }
+
+ 
+
+
+ 
 export default App;
